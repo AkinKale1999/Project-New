0c8a26a1b660b11bd8620c879b7578c46ab31e5f
Dein Name <deine.email@example.com>
2025-01-14 09:08:02 +0100

    New Features

---
+++ src/components/GithubAndGoogleIcon/GithubGoogleIcon.jsx
@@ -0,0 +1,7 @@
+export default function IconGithubAndGoogle({src, alt}) {
+
+
+    return (<>
+        <img className="GithubGoogleIcon" src={src} alt={alt} />
+    </>)
+}
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../Button/Button";
 import InputFields from "../InputFields/InputFields";
+import IconGithubAndGoogle from "../GithubAndGoogleIcon/GithubGoogleIcon";
 
 function Login() {
   const [Username, setUsername] = useState("");
@@ -75,14 +76,33 @@ function Login() {
       <div id="MainContainerLogin" ref={Main_Container}>
         <div id="LoginContainer">
           <form action="/login" id="LoginForm" onSubmit={handleLogin}>
-            <h1 id="LoginHeader"yy>Anmelden</h1>
+            <h1 id="LoginHeader">Login</h1>
+
+            <div className="LoginWithGithubOrGoogle">
+              <form action="">
+                <IconGithubAndGoogle src={"/img/github.png"} alt={"Github-Icon"} />
+                <Button text={"GitHub anmelden"}>
+                </Button>
+              </form>
+
+              <form action="">
+                <IconGithubAndGoogle src={"/img/github.png"} alt={"Google-Icon"} />
+                <Button text={"Google anmelden"}>
+                </Button>
+              </form>
+            </div>
+
+            <div className="BreakpointLogins">
+              <span className="OrDifferentLogin">oder</span>
+
+            </div>
 
 
             <InputFields type={"text"} id={"UserLogin"} placeholder={"Email *"} value={Username} onChange={(e) => setUsername(e.target.value)} required={"required"} />
 
             <InputFields type={"password"} id={"PasswordLogin"} placeholder={"Password *"} value={Password} onChange={(e) => setPassword(e.target.value)} required={"required"} />
 
-            <Button btnType={"submit"} className={""} text={"Login"} disabled={buttonText} />
+            <Button btnType={"submit"} id="BtnLogin" text={"Login"} disabled={buttonText} />
             {/* disabled={buttonText} bedeutet das wenn buttonText = true ist das man mit dem Button nicht 
             Interagieren kann, wenn er false ist dann schon*/}
 
