7d7e0172d04c00e7dc51e6282dce40d7bf3c6bd3
Dein Name <deine.email@example.com>
2025-01-16 01:30:53 +0100

    Responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ export default function Navbar() {
 
           {/* ------------------------------------ */}
 
-          <Link to="/Warenkorb" >
+          <Link to="/warenkorb" >
             <ShoppingBagIcon className="NavbarElements" />
           </Link>
 
